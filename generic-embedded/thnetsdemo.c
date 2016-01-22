ebbf348eb35ac42da1efa5c1ea334d925933fe43
Marko Vitez <marko@vitez.it>
2016-01-22 22:06:27 +0100

    Add motion mode

--- generic-embedded/thnetsdemo.c
+++ generic-embedded/thnetsdemo.c
@@ -11,11 +11,22 @@
 #include "libgl.h"
 #include "videocap.h"
 
+#define MIN(X, Y) ((X) < (Y) ? (X) : (Y))
+#define MAX(X, Y) ((X) > (Y) ? (X) : (Y))
 #define NBUFFERS 4
+#define MAX_OBJECTS 20
 static void *vcap;
 static int frame_width, frame_height, win_width, win_height;
+static THNETWORK *net;
 static char *frames[NBUFFERS];
 static int curframe;
+const int eye = 231;
+const int motion_threshold = 20;
+const int motion_downscale = 8;
+const int motion_minsize = 64;
+const int decaylimit = 3;
+const int minstillframes = 3;
+const float pdt = 0.5;
 
 #define MAXITEMS 100
 static int nitems = 0;
@@ -33,7 +44,7 @@ static struct ITEM items[MAXITEMS];
 static FT_Library ft_lib;
 static FT_Face ft_face;
 
-static int yuyv2rgb(unsigned char *src, int srcw, int srch, int src_stride, unsigned char *rgb, int dstw, int dsth)
+static int rescale(unsigned char *src, int srcw, int srch, int src_stride, int format, unsigned char *dst, int dstw, int dsth, int dst_stride)
 {
 	struct SwsContext *sws_ctx;
 	const uint8_t *srcslice[3];
@@ -42,10 +53,10 @@ static int yuyv2rgb(unsigned char *src, int srcw, int srch, int src_stride, unsi
 
 	srcslice[0] = src;
 	srcstride[0] = src_stride;
-	dstslice[0] = rgb;
-	dststride[0] = 3 * dstw;
+	dstslice[0] = dst;
+	dststride[0] = dst_stride;
 
-	sws_ctx = sws_getContext(srcw, srch, AV_PIX_FMT_YUYV422, dstw, dsth, AV_PIX_FMT_RGB24, SWS_FAST_BILINEAR, 0, 0, 0);
+	sws_ctx = sws_getContext(srcw, srch, AV_PIX_FMT_YUYV422, dstw, dsth, format, SWS_FAST_BILINEAR, 0, 0, 0);
 	sws_scale(sws_ctx, srcslice, srcstride, 0, srch, dstslice, dststride);
 	sws_freeContext(sws_ctx);
 	return 0;
@@ -247,13 +258,317 @@ int loadcategories(const char *modelsdir)
 	return 0;
 }
 
+void dilate(unsigned char *dst, unsigned char *src, int w, int h, int size)
+{
+	int x, y, x1, x2, y1, y2;
+	
+	memset(dst, 0, w*h);
+	for(y = 0; y < h; y++)
+		for(x = 0; x < w; x++)
+			if(src[x + w*y])
+			{
+				y1 = y - size/2;
+				if(y1 < 0)
+					y1 = 0;
+				y2 = y + size/2;
+				if(y2 > h)
+					y2 = h;
+				while(y1 < y2)
+				{
+					x1 = x - size/2;
+					if(x1 < 0)
+						x1 = 0;
+					x2 = x + size/2;
+					if(x2 > w)
+						x2 = w;
+					memset(dst + y1*w + x1, 1, x2-x1);
+					y1++;
+				}
+			}
+}
+
+void erode(unsigned char *dst, unsigned char *src, int w, int h, int size)
+{
+	int x, y, x1, x2, y1, y2;
+	
+	memset(dst, 1, w*h);
+	for(y = 0; y < h; y++)
+		for(x = 0; x < w; x++)
+			if(!src[x + w*y])
+			{
+				y1 = y - size/2;
+				if(y1 < 0)
+					y1 = 0;
+				y2 = y + size/2;
+				if(y2 > h)
+					y2 = h;
+				while(y1 < y2)
+				{
+					x1 = x - size/2;
+					if(x1 < 0)
+						x1 = 0;
+					x2 = x + size/2;
+					if(x2 > w)
+						x2 = w;
+					memset(dst + y1*w + x1, 0, x2-x1);
+					y1++;
+				}
+			}
+}
+
+int connectedComponent(unsigned char *image, int* coordinates, int coordinatesSize, int height, int width);
+
+void expandrect(RECT *r, int minsize, int image_width, int image_height)
+{
+	if(r->w < minsize)
+	{
+		r->x -= (minsize - r->w) / 2;
+		r->w = minsize;
+	}
+	if(r->h < minsize)
+	{
+		r->y -= (minsize - r->h) / 2;
+		r->h = minsize;
+	}
+	// Expand the motion rectangle to be a square
+	if(r->w < r->h)
+	{
+		r->x -= (r->h - r->w) / 2;
+		r->w = r->h;
+	} else {
+		r->y -= (r->w - r->h) / 2;
+		r->h = r->w;
+	}
+	// Reduce the square, if it's higher of the frame
+	if(r->h > image_height)
+	{
+		r->x += (r->h - image_height) / 2;
+		r->y += (r->h - image_height) / 2;
+		r->w = r->h = image_height;
+	}
+	// Put the square inside the frame
+	if(r->x + r->w > image_width)
+		r->x = image_width - r->w;
+	else if(r->x < 0)
+		r->x = 0;
+	if(r->y + r->h > image_height)
+		r->y = image_height - r->h;
+	else if(r->y < 0)
+		r->y = 0;
+}
+
+int nobjects;
+unsigned oids;
+struct object {
+	RECT r;
+	unsigned id;
+	unsigned color;
+	char target, valid, decay, still, permastill;
+} objects[MAX_OBJECTS];
+
+float *process(RECT r, unsigned char *rgb)
+{
+	float *result;
+	int outwidth, outheight;
+
+	expandrect(&r, motion_minsize, frame_width, frame_height);
+	rescale((unsigned char *)frames[curframe] + 2 * (r.x + r.y * frame_width),
+		r.w, r.h, frame_width * 2, AV_PIX_FMT_RGB24, rgb, eye, eye, 3 * eye);
+	THProcessImages(net, &rgb, 1, eye, eye, 3*eye, &result, &outwidth, &outheight, 0);
+	return result;
+}
+
+void run_motion()
+{
+	unsigned char *y[2], *diffs[4];
+	int cury = 0;
+	int rw = frame_width / motion_downscale, rh = frame_height / motion_downscale;
+	int i, j, n;
+	RECT rects[10], *r;
+	float *result;
+	int personidx = -1;
+	unsigned char *rgb = malloc(eye * eye * 3);
+	const unsigned colors[] = {
+		0xffffff, 0x0000ff, 0x00ff00, 0x008000, 0xff8000, 0x00ffff,
+		0x008080, 0xff00ff, 0x800080, 0x8000ff, 0x808000,
+		0x808080, 0x404040, 0xff0000, 0x800000, 0xffff00, 0x808000};
+		
+	
+	y[0] = malloc(rw * rh);
+	y[1] = malloc(rw * rh);
+	for(i = 0; i < 4; i++)
+		diffs[i] = malloc(rw * rh);
+	// diffs[0] and diffs[1] contain the last two binary images
+	// diffs[2] contains the sum of diffs[0] and diffs[1] and then the eroded
+	// diffs[3] contains the dilated sum
+	for(i = 0; i < ncat; i++)
+		if(!strcmp(categories[i], "person"))
+		{
+			personidx = i;
+			break;
+		}
+	if(personidx == -1)
+		THError("Fatal: this network does not contain a category called person");
+	for(;;)
+	{
+		rescale((unsigned char *)frames[curframe], frame_width, frame_height, frame_width * 2, AV_PIX_FMT_GRAY8, y[cury], rw, rh, rw);
+		for(i = 0; i < rw*rh; i++)
+		{
+			diffs[cury][i] = abs(y[cury][i] - y[1-cury][i]) > motion_threshold;
+			diffs[2][i] = diffs[0][i] + diffs[1][i];
+		}
+		dilate(diffs[3], diffs[2], rw, rh, 9);
+		erode(diffs[2], diffs[3], rw, rh, 5);
+		n = connectedComponent(diffs[2], (int *)rects, sizeof(rects)/sizeof(rects[0]), rh, rw);
+		
+		for(i = 0; i < n; i++)
+		{
+			// Bring the rectangle to actual frame view
+			r = rects + i;
+			r->w = (r->w - r->x + 3) * motion_downscale;
+			r->h = (r->h - r->y + 3) * motion_downscale;
+			r->x = (r->x - 1) * motion_downscale;
+			r->y = (r->y - 1) * motion_downscale;
+			if(r->w < motion_minsize || r->h < motion_minsize)
+				continue;
+			result = process(*r, rgb);
+			int b2o = -1;
+			for(j = 0; j < nobjects; j++)
+			{
+				RECT *r2 = &objects[j].r;
+				int x1 = MAX(r->x, r2->x);
+				int x2 = MIN(r->x + r->w, r2->x + r2->w);
+				int y1 = MAX(r->y, r2->y);
+				int y2 = MIN(r->y + r->h, r2->y + r2->h);
+				int minarea = MIN(r->w * r->h, r2->w * r2->h);
+				float overlap = abs(y2-y1) * abs(x2-x1) / (float)minarea;
+				if(overlap > 0.5 && y2 - y1 > 0 && x2 - x1 > 0)
+				{
+					if(b2o >= 0)
+					{
+						memmove(objects + b2o, objects + b2o + 1, (nobjects - b2o - 1) * sizeof(*objects));
+						nobjects--;
+						j--;
+					} else {
+						b2o = j;
+						// If the new bounding box area suddenly decreases
+						// the object is probably becoming still, as only some
+						// parts are still in motion
+						if(r2->w * r2->h / (float)(r->w * r->h) > 2)
+							r = r2;
+					}
+				}
+			}
+			char target = result[personidx] >= pdt;
+			struct object *o = b2o >= 0 ? objects + b2o : 0;
+			if(!o)
+			{
+				// New object
+				o = objects + nobjects++;
+				o->color = 0;
+				o->id = oids++;
+				o->r = *r;
+				o->valid = 1;
+				o->decay = decaylimit;
+				o->still = 0;
+				o->permastill = 0;
+				o->target = target;
+			} else {
+				if(o->still)
+					o->valid = 0;
+				else {
+					o->valid = 1;
+					o->r = *r;
+				}
+				o->decay = decaylimit;
+				if(target && !o->target)
+				{
+					o->id = oids++;
+					o->target = 1;
+				}
+			}
+		}
+		for(j = 0; j < nobjects; j++)
+		{
+			struct object *o = objects + j;
+			if(o->valid)
+				o->valid = 0;
+			else {
+				RECT *r = &o->r;
+				result = process(*r, rgb);
+				char target = result[personidx] >= pdt;
+				if(target)
+					o->still = o->permastill > minstillframes;
+				else if(!o->decay)
+				{
+					memmove(objects + j, objects + j + 1, (nobjects - j - 1) * sizeof(*objects));
+					nobjects--;
+					j--;
+				} else o->decay--;
+				o->permastill++;
+			}
+		}
+		nitems = 0;
+		for(j = 0; j < nobjects; j++)
+		{
+			struct object *o = objects + j;
+			RECT *r = &o->r;
+			if(o->target)
+			{
+				if(!o->color)
+					o->color = colors[rand() * sizeof(colors) / sizeof(colors[0]) / RAND_MAX];
+				rectangle(r->x, r->y, r->w, r->h, 8, o->color);
+				text(r->x + 8, r->y + 8, "person", 16, o->color);
+			} else rectangle(r->x, r->y, r->w, r->h, 2, 0x808080);
+		}
+		cury = 1-cury;
+	}
+}
+
+void run_simple()
+{
+	unsigned char *rgb = malloc(frame_width * frame_height * 3);
+	int i, offset = (frame_width - frame_height) / 2 * 2;
+	struct catp *res = malloc(sizeof(*res) * ncat);
+	
+	for(;;)
+	{
+		float *result;
+		double t;
+		int outwidth, outheight, n;
+		char s[300];
+
+		t = seconds();
+		rescale((unsigned char *)frames[curframe] + offset, frame_height, frame_height, frame_width * 2, AV_PIX_FMT_RGB24, rgb, eye, eye, 3 * eye);
+		n = THProcessImages(net, &rgb, 1, eye, eye, 3*eye, &result, &outwidth, &outheight, 0);
+		if(n / outwidth != ncat)
+			THError("Bug: wrong number of outputs received: %d != %d", n / outwidth, ncat);
+		if(outheight != 1)
+			THError("Bug: outheight expected 1");
+		t = 1.0 / (seconds() - t);
+		for(i = 0; i < ncat; i++)
+		{
+			res[i].p = result[i];
+			res[i].cat = categories[i];
+		}
+		qsort(res, ncat, sizeof(*res), catpcmp);
+		nitems = 0;
+		sprintf(s, "%.2f fps", t);
+		text(10, 10, s, 16, 0xff0000);
+		for(i = 0; i < 5; i++)
+		{
+			text(10, 40 + i * 20, res[i].cat, 16, 0x00a000);
+			sprintf(s, "(%.0f %%)", res[i].p * 100);
+			text(100, 40 + i * 20, s, 16, 0x00a000);
+		}
+	}
+}
+
 int main(int argc, char **argv)
 {
-	THNETWORK *net;
-	int rc, alg = 1, i, fullscreen = 0;
+	int rc, alg = 1, i, fullscreen = 0, motion = 0;
 	char camera[6] = "cam0";
 	const char *modelsdir = 0, *inputfile = camera;
-	const int eye = 231;
 
 	frame_width = 640;
 	frame_height = 480;
@@ -277,6 +592,9 @@ int main(int argc, char **argv)
 			break;
 		case 'f':
 			fullscreen = 1;
+			break;
+		case 'M':
+			motion = 1;
 			break;
 		case 'r':
 			if(i+1 < argc)
@@ -304,6 +622,7 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Syntax: thnetsdemo -m <models directory> [-i <input file (default cam0)>]\n");
 		fprintf(stderr, "                   [-a <alg=0:norm,1:MM,default,2:cuDNN,3:cudNNhalf>]\n");
 		fprintf(stderr, "                   [-r <QVGA,VGA (default),HD,FHD] [-f(ullscreen)]\n");
+		fprintf(stderr, "                   [-M(otion mode)\n");
 		return -1;
 	}
 	if(alg == 3)
@@ -349,42 +668,11 @@ int main(int argc, char **argv)
 		pthread_t tid;
 		pthread_create(&tid, 0, rendering_thread, 0);
 		pthread_detach(tid);
-		unsigned char *rgb = malloc(frame_width * frame_height * 3);
-		int offset = (frame_width - frame_height) / 2 * 2;
-		struct catp *res = malloc(sizeof(*res) * ncat);
 		while(!frames[curframe])
 			usleep(10000);
-		for(;;)
-		{
-			float *result;
-			double t;
-			int outwidth, outheight, n;
-			char s[300];
-
-			t = seconds();
-			yuyv2rgb((unsigned char *)frames[curframe] + offset, frame_height, frame_height, frame_width * 2, (unsigned char *)rgb, eye, eye);
-			n = THProcessImages(net, &rgb, 1, eye, eye, 3*eye, &result, &outwidth, &outheight);
-			if(n / outwidth != ncat)
-				THError("Bug: wrong number of outputs received: %d != %d", n / outwidth, ncat);
-			if(outheight != 1)
-				THError("Bug: outheight expected 1");
-			t = 1.0 / (seconds() - t);
-			for(i = 0; i < ncat; i++)
-			{
-				res[i].p = result[i];
-				res[i].cat = categories[i];
-			}
-			qsort(res, ncat, sizeof(*res), catpcmp);
-			nitems = 0;
-			sprintf(s, "%.2f fps", t);
-			text(10, 10, s, 16, 0xff0000);
-			for(i = 0; i < 5; i++)
-			{
-				text(10, 40 + i * 20, res[i].cat, 16, 0x00a000);
-				sprintf(s, "(%.0f %%)", res[i].p * 100);
-				text(100, 40 + i * 20, s, 16, 0x00a000);
-			}
-		}
+		if(motion)
+			run_motion();
+		else run_simple();
 	} else printf("The network could not be loaded: %d\n", THLastError());
 	return 0;
 }
--- opencv/thnetsopencvdemo.cpp
+++ opencv/thnetsopencvdemo.cpp
@@ -14,9 +14,23 @@ extern "C" {
 using namespace cv;
 
 #define NBUFFERS 4
+#define MAX_OBJECTS 20
 static int frame_width, frame_height, win_width, win_height;
 static FT_Library ft_lib;
 static FT_Face ft_face;
+static THNETWORK *net;
+const char *winname = "thnets opencv demo";
+const int eye = 231;
+const int motion_threshold = 20;
+const int motion_downscale = 8;
+const int motion_minsize = 64;
+const int decaylimit = 3;
+const int minstillframes = 3;
+const float pdt = 0.5;
+
+typedef struct {
+	int x, y, w, h;
+} RECT;
 
 static double seconds()
 {
@@ -123,14 +137,339 @@ static int text(Mat *frame, int x, int y, const char *text, int size, int color)
 	return 0;
 }
 
+void dilate(unsigned char *dst, unsigned char *src, int w, int h, int size)
+{
+	int x, y, x1, x2, y1, y2;
+	
+	memset(dst, 0, w*h);
+	for(y = 0; y < h; y++)
+		for(x = 0; x < w; x++)
+			if(src[x + w*y])
+			{
+				y1 = y - size/2;
+				if(y1 < 0)
+					y1 = 0;
+				y2 = y + size/2;
+				if(y2 > h)
+					y2 = h;
+				while(y1 < y2)
+				{
+					x1 = x - size/2;
+					if(x1 < 0)
+						x1 = 0;
+					x2 = x + size/2;
+					if(x2 > w)
+						x2 = w;
+					memset(dst + y1*w + x1, 1, x2-x1);
+					y1++;
+				}
+			}
+}
+
+void erode(unsigned char *dst, unsigned char *src, int w, int h, int size)
+{
+	int x, y, x1, x2, y1, y2;
+	
+	memset(dst, 1, w*h);
+	for(y = 0; y < h; y++)
+		for(x = 0; x < w; x++)
+			if(!src[x + w*y])
+			{
+				y1 = y - size/2;
+				if(y1 < 0)
+					y1 = 0;
+				y2 = y + size/2;
+				if(y2 > h)
+					y2 = h;
+				while(y1 < y2)
+				{
+					x1 = x - size/2;
+					if(x1 < 0)
+						x1 = 0;
+					x2 = x + size/2;
+					if(x2 > w)
+						x2 = w;
+					memset(dst + y1*w + x1, 0, x2-x1);
+					y1++;
+				}
+			}
+}
+
+extern "C" int connectedComponent(unsigned char *image, int* coordinates, int coordinatesSize, int height, int width);
+
+void expandrect(RECT *r, int minsize, int image_width, int image_height)
+{
+	if(r->w < minsize)
+	{
+		r->x -= (minsize - r->w) / 2;
+		r->w = minsize;
+	}
+	if(r->h < minsize)
+	{
+		r->y -= (minsize - r->h) / 2;
+		r->h = minsize;
+	}
+	// Expand the motion rectangle to be a square
+	if(r->w < r->h)
+	{
+		r->x -= (r->h - r->w) / 2;
+		r->w = r->h;
+	} else {
+		r->y -= (r->w - r->h) / 2;
+		r->h = r->w;
+	}
+	// Reduce the square, if it's higher of the frame
+	if(r->h > image_height)
+	{
+		r->x += (r->h - image_height) / 2;
+		r->y += (r->h - image_height) / 2;
+		r->w = r->h = image_height;
+	}
+	// Put the square inside the frame
+	if(r->x + r->w > image_width)
+		r->x = image_width - r->w;
+	else if(r->x < 0)
+		r->x = 0;
+	if(r->y + r->h > image_height)
+		r->y = image_height - r->h;
+	else if(r->y < 0)
+		r->y = 0;
+}
+
+int nobjects;
+unsigned oids;
+struct object {
+	RECT r;
+	unsigned id;
+	unsigned color;
+	char target, valid, decay, still, permastill;
+} objects[MAX_OBJECTS];
+
+float *process(RECT r, Mat frame, unsigned char *rgb)
+{
+	float *result;
+	int outwidth, outheight;
+
+	expandrect(&r, motion_minsize, frame_width, frame_height);
+	Rect roi(r.x, r.y, r.w, r.h);
+	Mat cropped = frame(roi);
+	Mat resized;
+	resize(frame, resized, Size(eye, eye));
+	THProcessImages(net, &resized.data, 1, eye, eye, 3*eye, &result, &outwidth, &outheight, 1);
+	return result;
+}
+
+void run_motion(VideoCapture &cap)
+{
+	Mat frame, y[2];
+	unsigned char *diffs[4];
+	int cury = 0;
+	int rw = frame_width / motion_downscale, rh = frame_height / motion_downscale;
+	int i, j, n;
+	RECT rects[10], *r;
+	float *result;
+	int personidx = -1;
+	unsigned char *rgb = (unsigned char *)malloc(eye * eye * 3);
+	const unsigned colors[] = {
+		0xffffff, 0x0000ff, 0x00ff00, 0x008000, 0xff8000, 0x00ffff,
+		0x008080, 0xff00ff, 0x800080, 0x8000ff, 0x808000,
+		0x808080, 0x404040, 0xff0000, 0x800000, 0xffff00, 0x808000};
+		
+	for(i = 0; i < 4; i++)
+		diffs[i] = (unsigned char *)malloc(rw * rh);
+	// diffs[0] and diffs[1] contain the last two binary images
+	// diffs[2] contains the sum of diffs[0] and diffs[1] and then the eroded
+	// diffs[3] contains the dilated sum
+	for(i = 0; i < ncat; i++)
+		if(!strcmp(categories[i], "person"))
+		{
+			personidx = i;
+			break;
+		}
+	if(personidx == -1)
+		THError("Fatal: this network does not contain a category called person");
+	for(;;)
+	{
+		cap >> frame;
+		Mat resized;
+		resize(frame, resized, Size(rw, rh));
+		cvtColor(resized, y[cury], CV_BGR2GRAY);
+		if(!y[1-cury].data)
+		{
+			cury = 1-cury;
+			continue;
+		}
+		for(i = 0; i < rw*rh; i++)
+		{
+			diffs[cury][i] = abs(y[cury].data[i] - y[1-cury].data[i]) > motion_threshold;
+			diffs[2][i] = diffs[0][i] + diffs[1][i];
+		}
+		dilate(diffs[3], diffs[2], rw, rh, 9);
+		erode(diffs[2], diffs[3], rw, rh, 5);
+		n = connectedComponent(diffs[2], (int *)rects, sizeof(rects)/sizeof(rects[0]), rh, rw);
+		
+		for(i = 0; i < n; i++)
+		{
+			// Bring the rectangle to actual frame view
+			r = rects + i;
+			r->w = (r->w - r->x + 3) * motion_downscale;
+			r->h = (r->h - r->y + 3) * motion_downscale;
+			r->x = (r->x - 1) * motion_downscale;
+			r->y = (r->y - 1) * motion_downscale;
+			if(r->w < motion_minsize || r->h < motion_minsize)
+				continue;
+			result = process(*r, frame, rgb);
+
+			int b2o = -1;
+			for(j = 0; j < nobjects; j++)
+			{
+				RECT *r2 = &objects[j].r;
+				int x1 = MAX(r->x, r2->x);
+				int x2 = MIN(r->x + r->w, r2->x + r2->w);
+				int y1 = MAX(r->y, r2->y);
+				int y2 = MIN(r->y + r->h, r2->y + r2->h);
+				int minarea = MIN(r->w * r->h, r2->w * r2->h);
+				float overlap = abs(y2-y1) * abs(x2-x1) / (float)minarea;
+				if(overlap > 0.5 && y2 - y1 > 0 && x2 - x1 > 0)
+				{
+					if(b2o >= 0)
+					{
+						memmove(objects + b2o, objects + b2o + 1, (nobjects - b2o - 1) * sizeof(*objects));
+						nobjects--;
+						j--;
+					} else {
+						b2o = j;
+						// If the new bounding box area suddenly decreases
+						// the object is probably becoming still, as only some
+						// parts are still in motion
+						if(r2->w * r2->h / (float)(r->w * r->h) > 2)
+							r = r2;
+					}
+				}
+			}
+			char target = result[personidx] >= pdt;
+			struct object *o = b2o >= 0 ? objects + b2o : 0;
+			if(!o)
+			{
+				// New object
+				o = objects + nobjects++;
+				o->color = 0;
+				o->id = oids++;
+				o->r = *r;
+				o->valid = 1;
+				o->decay = decaylimit;
+				o->still = 0;
+				o->permastill = 0;
+				o->target = target;
+			} else {
+				if(o->still)
+					o->valid = 0;
+				else {
+					o->valid = 1;
+					o->r = *r;
+				}
+				o->decay = decaylimit;
+				if(target && !o->target)
+				{
+					o->id = oids++;
+					o->target = 1;
+				}
+			}
+		}
+		for(j = 0; j < nobjects; j++)
+		{
+			struct object *o = objects + j;
+			if(o->valid)
+				o->valid = 0;
+			else {
+				RECT *r = &o->r;
+				result = process(*r, frame, rgb);
+				char target = result[personidx] >= pdt;
+				if(target)
+					o->still = o->permastill > minstillframes;
+				else if(!o->decay)
+				{
+					memmove(objects + j, objects + j + 1, (nobjects - j - 1) * sizeof(*objects));
+					nobjects--;
+					j--;
+				} else o->decay--;
+				o->permastill++;
+			}
+		}
+		for(j = 0; j < nobjects; j++)
+		{
+			struct object *o = objects + j;
+			RECT *r = &o->r;
+			Rect r1(r->x, r->y, r->w, r->h);
+			if(o->target)
+			{
+				if(!o->color)
+					o->color = colors[rand() * sizeof(colors) / sizeof(colors[0]) / RAND_MAX];
+				Scalar col((o->color >> 16) &  0xff, (o->color >> 8) & 0xff, o->color & 0xff);
+				rectangle(frame, r1, col, 8);
+				text(&frame, r->x + 8, r->y + 8, "person", 16, o->color);
+			} else {
+				Scalar col(0x80, 0x80, 0x80);
+				rectangle(frame, r1, col, 1);
+			}
+		}
+		imshow(winname, frame);
+		waitKey(1);
+		cury = 1-cury;
+	}
+}
+
+void run_simple(VideoCapture &cap)
+{
+	int offset = (frame_width - frame_height) / 2;
+	struct catp *res = (struct catp *)malloc(sizeof(*res) * ncat);
+	int i;
+	
+	Mat frame;
+	float fps = 1;
+	for(;;)
+	{
+		float *result;
+		double t;
+		int outwidth, outheight, n;
+		char s[300];
+
+		t = seconds();
+		cap >> frame;
+		Rect roi(offset, 0, frame_height, frame_height);
+		Mat cropped = frame(roi);
+		Mat resized;
+		resize(frame, resized, Size(eye, eye));
+		n = THProcessImages(net, &resized.data, 1, eye, eye, 3*eye, &result, &outwidth, &outheight, 1);
+		if(n / outwidth != ncat)
+			THError("Bug: wrong number of outputs received: %d != %d", n / outwidth, ncat);
+		if(outheight != 1)
+			THError("Bug: outheight expected 1");
+		for(i = 0; i < ncat; i++)
+		{
+			res[i].p = result[i];
+			res[i].cat = categories[i];
+		}
+		qsort(res, ncat, sizeof(*res), catpcmp);
+		sprintf(s, "%.2f fps", fps);
+		text(&cropped, 10, 10, s, 16, 0xff0000);
+		for(i = 0; i < 5; i++)
+		{
+			text(&cropped, 10, 40 + i * 20, res[i].cat, 16, 0x00a000);
+			sprintf(s, "(%.0f %%)", res[i].p * 100);
+			text(&cropped, 100, 40 + i * 20, s, 16, 0x00a000);
+		}
+		imshow(winname, cropped);
+		waitKey(1);
+		fps = 1.0 / (seconds() - t);
+	}
+}
+
 int main(int argc, char **argv)
 {
-	THNETWORK *net;
-	int alg = 1, i;
+	int alg = 1, i, motion = 0;
 	char camera[6] = "cam0";
 	const char *modelsdir = 0, *inputfile = camera;
-	const int eye = 231;
-	const char *winname = "thnets opencv demo";
 
 	frame_width = 640;
 	frame_height = 480;
@@ -152,6 +491,9 @@ int main(int argc, char **argv)
 		case 'a':
 			if(i+1 < argc)
 				alg = atoi(argv[++i]);
+			break;
+		case 'M':
+			motion = 1;
 			break;
 		case 'r':
 			if(i+1 < argc)
@@ -179,6 +521,7 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Syntax: thnetsdemo -m <models directory> [-i <input file (default cam0)>]\n");
 		fprintf(stderr, "                   [-a <alg=0:norm,1:MM,default,2:cuDNN,3:cudNNhalf>]\n");
 		fprintf(stderr, "                   [-r <QVGA,VGA (default),HD,FHD] [-f(ullscreen)]\n");
+		fprintf(stderr, "                   [-M(otion mode)\n");
 		return -1;
 	}
 	if(alg == 3)
@@ -211,47 +554,9 @@ int main(int argc, char **argv)
 		cap.set(CV_CAP_PROP_FRAME_HEIGHT, frame_height);
 		win_width = frame_width;
 		win_height = frame_height;
-		int offset = (frame_width - frame_height) / 2;
-		struct catp *res = (struct catp *)malloc(sizeof(*res) * ncat);
-		
-		Mat frame;
-		float fps = 1;
-		for(;;)
-		{
-			float *result;
-			double t;
-			int outwidth, outheight, n;
-			char s[300];
-
-			t = seconds();
-			cap >> frame;
-			Rect roi(offset, 0, frame_height, frame_height);
-			Mat cropped = frame(roi);
-			Mat resized;
-			resize(frame, resized, Size(eye, eye));
-			n = THProcessImages(net, &resized.data, 1, eye, eye, 3*eye, &result, &outwidth, &outheight);
-			if(n / outwidth != ncat)
-				THError("Bug: wrong number of outputs received: %d != %d", n / outwidth, ncat);
-			if(outheight != 1)
-				THError("Bug: outheight expected 1");
-			for(i = 0; i < ncat; i++)
-			{
-				res[i].p = result[i];
-				res[i].cat = categories[i];
-			}
-			qsort(res, ncat, sizeof(*res), catpcmp);
-			sprintf(s, "%.2f fps", fps);
-			text(&cropped, 10, 10, s, 16, 0xff0000);
-			for(i = 0; i < 5; i++)
-			{
-				text(&cropped, 10, 40 + i * 20, res[i].cat, 16, 0x00a000);
-				sprintf(s, "(%.0f %%)", res[i].p * 100);
-				text(&cropped, 100, 40 + i * 20, s, 16, 0x00a000);
-			}
-			imshow(winname, cropped);
-			waitKey(1);
-			fps = 1.0 / (seconds() - t);
-		}
+		if(motion)
+			run_motion(cap);
+		else run_simple(cap);
 	} else printf("The network could not be loaded: %d\n", THLastError());
 	return 0;
 }
